448c508734acad9cb722ea80d12aa7cf9d3fbc22
Thiago Esterci Fernandes <thiago.esterci@engenharia.ufjf.br>
2021-08-30 20:06:09 -0300

    2021_08_30_update

---
+++ Python/Anomaly_Detection/hyper_parameters_study/autoencoder_Dijets.py
@@ -0,0 +1,249 @@
+from tensorflow.python.framework.ops import disable_eager_execution
+disable_eager_execution()
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow.keras import backend as K
+from Disco_tensor_flow import decorr
+from keras.optimizers import Adam
+from datetime import datetime
+import argparse
+import pickle as pk
+
+
+
+def time_stamp():
+    dateTimeObj = datetime.now()
+    timestampStr = dateTimeObj.strftime("%d-%b-%Y-%H.%M.%S")
+    return timestampStr
+
+
+
+parser = argparse.ArgumentParser(description = '', add_help = False)
+parser = argparse.ArgumentParser()
+
+
+parser.add_argument('-b','--batch_size', action='store',
+        dest='batch_size', required = True,
+            help = "The job config file that will be used to configure the job (sort and init).")
+
+parser.add_argument('-e','--encoding_dim', action='store',
+        dest='encoding_dim', required = False, default = None,
+            help = "The volume output.")
+
+parser.add_argument('-l','--lambda_disco', action='store',
+        dest='lambda_disco', required = True,
+            help = "The job config file that will be used to configure the job (sort and init).")
+
+parser.add_argument('-a1','--act_1', action='store',
+        dest='act_1', required = False, default = None,
+            help = "The volume output.")
+
+parser.add_argument('-a2','--act_2', action='store',
+        dest='act_2', required = False, default = None,
+            help = "The volume output.")
+
+parser.add_argument('-a3','--act_3', action='store',
+        dest='act_3', required = False, default = None,
+            help = "The volume output.")
+
+parser.add_argument('-f','--file', action='store',
+        dest='file', required = False, default = None,
+            help = "The volume output.")
+
+args = parser.parse_args()
+
+batch_size = int(args.batch_size)
+encoding_dim = int(args.encoding_dim)
+lambda_disco = int(args.lambda_disco)
+act_1 = args.act_1
+act_2 = args.act_2
+act_3 = args.act_3
+file = args.file
+
+it = file.split('__')[8]
+
+print(it)
+
+s = file
+
+with open(s, 'rb') as f:
+    data_dict = pk.load(f)
+
+train_data = data_dict['train_data']
+test_labels = data_dict['test_labels']
+test_data = data_dict['test_data']
+test_df = data_dict['test_df']
+
+# Fixed parameters
+
+nb_epoch = 150
+input_dim = train_data.shape[1]
+hidden_dim_1 = int(encoding_dim / 2)
+hidden_dim_2 = int(hidden_dim_1 / 2)
+learning_rate = 0.001
+
+##### Creates structure name #####
+
+struct_name = (
+    'batch_size' + '__' + str(batch_size) + '__' +
+    'encoding_dim' + '__' + str(encoding_dim) + '__' +
+    'lambda_disco' + '__' + str(lambda_disco) + '__' +
+    'act_1' + '__' + str(act_1) + '__' +
+    'act_2' + '__' + str(act_2) + '__' +
+    'act_3' + '__' + str(act_3) + '__' +
+    'it' + '__' + it + '__' + time_stamp()
+)
+
+###### Creates NN structure #####
+
+#input Layer
+input_layer = tf.keras.layers.Input(shape=(input_dim, ))
+sample_weights = tf.keras.layers.Input(shape=(1, ))
+#Encoder
+encoder = tf.keras.layers.Dense(encoding_dim, 
+                                activation=act_1,
+        activity_regularizer=tf.keras.regularizers.l2(learning_rate)
+                            )(input_layer)
+
+encoder = tf.keras.layers.Dropout(0.2)(encoder)
+
+encoder = tf.keras.layers.Dense(hidden_dim_1, 
+                                activation=act_2
+                            )(encoder)
+
+encoder = tf.keras.layers.Dense(hidden_dim_2, 
+                                activation=act_3
+                            )(encoder)
+# Decoder
+decoder = tf.keras.layers.Dense(hidden_dim_1, 
+                                activation=act_3
+                            )(encoder)
+
+decoder=tf.keras.layers.Dropout(0.2)(decoder)
+
+decoder = tf.keras.layers.Dense(encoding_dim, 
+                                activation=act_2
+                            )(decoder)
+
+decoder = tf.keras.layers.Dense(input_dim, 
+                                activation=act_1
+                            )(decoder)
+#Autoencoder
+autoencoder = tf.keras.Model(inputs=[input_layer, sample_weights], 
+                            outputs=decoder
+                            )
+
+#Defining early stop
+
+cp = tf.keras.callbacks.ModelCheckpoint(filepath="autoencoder.h5",
+                                        mode='min', 
+                                        monitor='val_loss', 
+                                        verbose=2, 
+                                        save_best_only=True
+                                    )
+
+early_stop = tf.keras.callbacks.EarlyStopping(
+    monitor='val_loss',
+    min_delta=0.0001,
+    patience=20,
+    verbose=1, 
+    mode='min',
+    restore_best_weights=True
+)
+
+# Compiling NN
+
+opt = Adam(lr=0.001)
+autoencoder.compile(optimizer=opt, 
+                    loss=decorr(input_layer[:,15], 
+                                decoder[:,15], 
+                                sample_weights[:,0],
+                                lambda_disco)
+                )
+
+# Training
+try:
+    Train = [train_data, np.ones(len(train_data))]
+
+    history = autoencoder.fit(x = Train, 
+                            y = train_data,
+                            epochs=nb_epoch,
+                            batch_size=batch_size,
+                            shuffle=True,
+                            validation_split=0.1,
+                            verbose=1,
+                            callbacks=[cp, early_stop]
+                            ).history
+    
+    # Ploting Model Loss
+
+    fig, ax = plt.subplots()
+    plt.plot(history['loss'], linewidth=2, label='Train')
+    plt.plot(history['val_loss'], linewidth=2, label='Test')
+    plt.legend(loc='upper right')
+    plt.title('Model loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    
+    fig.savefig('Figures/model-loss__' + struct_name + '__.png', 
+                bbox_inches='tight'
+            )
+    
+    # Predicting Test values
+    
+    start = datetime.now()
+
+    test_x_predictions = autoencoder.predict([test_data,
+                                            np.ones(len(test_data))]
+                                            )
+    # Calculating MSE
+
+    end = datetime.now()
+
+    mse = np.mean(np.power(test_data - test_x_predictions, 2), 
+                axis=1
+                )
+
+    # Creating MSE data frame
+
+    error_df = pd.DataFrame({'reconstruction_error': mse,
+                            'class': test_labels,
+                            'time' : end -start
+                            }
+                        )
+
+    # Creating 
+    # Concatenating with the original Attributes
+    
+    results_df = pd.concat([test_df,error_df],
+                        axis=1
+                        )
+    
+    # Saving Results
+    
+    results_df.to_csv('Results/results__' + struct_name + '__.csv')
+
+    # Ploting Reconstitution error
+    
+    groups = error_df.groupby('Class')
+    fig, ax = plt.subplots()
+    for name, group in groups:
+        ax.plot(group.index, group.Reconstruction_error, marker='o', ms=3.5, linestyle='',
+                label= "Signal" if name == 1 else "Background")
+    ax.legend()
+    plt.title("Reconstruction error")
+    plt.ylabel("Reconstruction error")
+    plt.xlabel("Data point index")
+    
+    fig.savefig('Figures/reconstruction-error__' + struct_name + '__.png', 
+                bbox_inches='tight'
+            )
+
+except:
+    with open('Results/Error__' + struct_name + '.txt', 'w') as f:
+        f.write('error')
+
+# Clear everything for the next init
+K.clear_session()        
---
+++ Python/Anomaly_Detection/hyper_parameters_study/run_jobs.py
@@ -0,0 +1,63 @@
+import os
+import glob
+import numpy as np
+
+# define the paths into the container
+data_path  = 'Data_Base/*'
+
+# Defining hyper-parameters range
+
+min_batch_size = 200
+
+max_batch_size = 500
+
+min_hidden_dim = 8
+
+max_hidden_dim = 21
+
+# Parameters in study
+
+batch_size_list = list(np.linspace(max_batch_size,min_batch_size,num=3,dtype=int))
+encoding_dim_list = list(np.linspace(min_hidden_dim,max_hidden_dim,num=3,dtype=int))
+lambda_disco_list = list(np.linspace(600,0,num=3,dtype=int))
+act_func_list_1 = ['relu',
+                   'sigmoid',
+                   #'softmax',
+                   #'softplus',
+                   #'softsign',
+                   'tanh',
+                   #'selu',
+                   #'elu',
+                   #'exponential'
+                   ]
+act_func_list_2 = act_func_list_1
+act_func_list_3 = act_func_list_1
+
+
+# create a list of config files
+file_list  = glob.glob(data_path)
+
+for file in file_list:
+    for batch_size in batch_size_list:
+        for encoding_dim in encoding_dim_list:
+            for lambda_disco in lambda_disco_list:
+                for act_1 in act_func_list_1:
+                    for act_2 in act_func_list_2:
+                        for act_3 in act_func_list_3:
+                            m_command = """python3 autoencoder_Dijets.py -b {BACH} \\
+                            -e {EDIM} \\
+                            -l {LAMBDA} \\
+                            -a1 {A1} \\
+                            -a2 {A2} \\
+                            -a3 {A3} \\
+                            -f {FILE}""".format(BACH=batch_size, 
+                                            EDIM=encoding_dim,
+                                            LAMBDA=lambda_disco,
+                                            A1=act_1,
+                                            A2=act_2,
+                                            A3=act_3,
+                                            FILE=file)
+
+                            print(m_command)
+                            # execute the tuning
+                            os.system(m_command)
